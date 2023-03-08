1288b36036f289cc79cbcd0b59b85499634c9963
gaoxiang <gaoxiang104@gmail.com>
2023-03-08 22:35:19 +0800

    gaoxiang : OnJava 学习笔记更新，第5章更新

---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/chapter05_control/WhileTest.java
@@ -0,0 +1,16 @@
+package org.gx.onjava.chapter05_control;
+
+public class WhileTest {
+    static boolean condition() {
+        boolean result = Math.random() < 0.99;
+        System.out.print(result + ", ");
+        return result;
+    }
+
+    public static void main(String[] args) {
+        while (condition()){
+            System.out.println("Inside 'while'");
+        }
+        System.out.println("Exited 'while'");
+    }
+}
