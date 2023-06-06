f9caf417710093bc9db914cfdffbe5c1c81f76ca
gaoxiang <gaoxiang104@gmail.com>
2023-06-06 16:50:06 +0800

    gaoxiang : On Java 第7章笔记更新

---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/chapter06_housekeeping/ArrayInit.java
@@ -0,0 +1,20 @@
+package org.gx.onjava.chapter06_housekeeping;
+
+import java.util.Arrays;
+
+// 数组初始化
+public class ArrayInit {
+    public static void main(String[] args) {
+        Integer[] a = {1,2,3,};
+        Integer[] b = new Integer[]{1,2,3,4,};
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+        //
+        f();
+    }
+
+    static void f(String... args){
+        System.out.println("f()");
+    }
+    
+}
