0cec2b8c289fef89c8542cd9ad1cafa9f6b699fb
gaoxiang <gaoxiang104@gmail.com>
2023-06-13 16:57:12 +0800

    gaoxiang : On Java 第8章笔记更新

---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/chapter08_reuse/Beetle.java
@@ -0,0 +1,43 @@
+package org.gx.onjava.chapter08_reuse;
+
+// 初始化的全过程
+class Insect { // 昆虫
+    private int i = 9;
+    protected int j;
+
+    Insect() {
+        System.out.println("i = " + i + ", j = " + j);
+        j = 39;
+    }
+
+    private static int x1 = printInit("static Insect.x1 initialized");
+
+    static int printInit(String s) {
+        System.out.println(s);
+        return 47;
+    }
+}
+
+public class Beetle extends Insect {
+    private int k = printInit("Beetle.k initialized");
+    public Beetle(){
+        System.out.println("k = " + k);
+        System.out.println("j = " + j);
+    }
+    private static int x2 = printInit("static Beetle.x2 initialized");
+
+    public static void main(String[] args) {
+        System.out.println("Beetle constructor");
+        Beetle b = new Beetle();
+    }
+}
+
+/* OUTPUT
+static Insect.x1 initialized
+static Beetle.x2 initialized
+Beetle constructor
+i = 9, j = 0
+Beetle.k initialized
+k = 47
+j = 39
+ */
