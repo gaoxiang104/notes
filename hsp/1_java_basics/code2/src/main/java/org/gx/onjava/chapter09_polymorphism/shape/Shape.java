b20844d7c081f85bf31f808536a42085834897c3
gaoxiang <gaoxiang104@gmail.com>
2023-06-20 17:46:20 +0800

    gaoxiang : On Java 第9章笔记更新

---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/chapter09_polymorphism/shape/Shape.java
@@ -0,0 +1,18 @@
+package org.gx.onjava.chapter09_polymorphism.shape;
+
+// 图形
+public class Shape {
+    public void draw() { // 绘制
+    }
+
+    public void erase() { // 擦除
+    }
+
+    public static void main(String[] args) {
+        RandomShapes gen = new RandomShapes();
+        // 执行多态方法调用
+        for (Shape shape : gen.array(9)) {
+            shape.draw();
+        }
+    }
+}
---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/chapter09_polymorphism/shape/Square.java
@@ -0,0 +1,14 @@
+package org.gx.onjava.chapter09_polymorphism.shape;
+
+// 正方形
+public class Square extends Shape{
+    @Override
+    public void draw() {
+        System.out.println("Square.draw()");
+    }
+
+    @Override
+    public void erase() {
+        System.out.println("Square.erase()");
+    }
+}
