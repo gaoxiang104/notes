b566829a7942373107f56ab71544ba37e8d4f345
gaoxiang <gaoxiang104@gmail.com>
2023-06-27 15:36:26 +0800

    gaoxiang : On Java 第10章笔记更新

---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/chapter10_interfaces/Operation.java
@@ -0,0 +1,16 @@
+package org.gx.onjava.chapter10_interfaces;
+
+// 操作接口
+public interface Operation {
+    void execute();
+
+    static void runOps(Operation... ops) {
+        for (Operation op : ops) {
+            op.execute();
+        }
+    }
+
+    static void show(String msg) {
+        System.out.println(msg);
+    }
+}
