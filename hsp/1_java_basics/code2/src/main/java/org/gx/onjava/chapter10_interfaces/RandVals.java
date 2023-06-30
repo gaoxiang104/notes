a539fe9d2848de2cee817595f850126fdc450781
gaoxiang <gaoxiang104@gmail.com>
2023-06-30 14:12:42 +0800

    gaoxiang : On Java 第10章笔记更新

---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/chapter10_interfaces/RandVals.java
@@ -0,0 +1,11 @@
+package org.gx.onjava.chapter10_interfaces;
+
+import java.util.Random;
+
+// 用非常量初始化表达式来初始化接口
+public interface RandVals {
+    Random RAND = new Random(47);
+    int RANDOM_INT = RAND.nextInt();
+    long RANDOM_LONG = RAND.nextLong();
+    double RANDOM_DOUBLE = RAND.nextDouble();
+}
---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/chapter10_interfaces/RandomDoubles.java
@@ -0,0 +1,19 @@
+package org.gx.onjava.chapter10_interfaces;
+
+import java.util.Random;
+
+public interface RandomDoubles {
+    Random RAND = new Random(47);
+
+    default double next() {
+        return RAND.nextDouble();
+    }
+
+    static void main(String[] args) {
+        RandomDoubles rd = new RandomDoubles() {
+        };
+        for (int i = 0; i < 7; i++) {
+            System.out.println(rd.next() + " ");
+        }
+    }
+}
