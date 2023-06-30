f6589e6f074f6262781f469ce1dd2cce739f9825
gaoxiang <gaoxiang104@gmail.com>
2023-06-30 15:29:11 +0800

    gaoxiang : On Java 第10章笔记更新

---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/chapter10_interfaces/Factories.java
@@ -0,0 +1,71 @@
+package org.gx.onjava.chapter10_interfaces;
+
+interface Service {
+    void method1();
+
+    void method2();
+}
+
+interface ServiceFactory {
+    Service getService();
+}
+
+class Service1 implements Service {
+    // 包访问权限
+    Service1() {
+    }
+
+    @Override
+    public void method1() {
+        System.out.println("Service1.method1()");
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("Service1.method2()");
+    }
+}
+
+class Service1Factory implements ServiceFactory {
+    @Override
+    public Service getService() {
+        return new Service1();
+    }
+}
+
+class Service2 implements Service {
+    // 包访问权限
+    Service2() {
+    }
+    @Override
+    public void method1() {
+        System.out.println("Service2.method1()");
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("Service2.method2()");
+    }
+}
+
+class Service2Factory implements ServiceFactory {
+    @Override
+    public Service getService() {
+        return new Service2();
+    }
+}
+
+public class Factories {
+    // 消费者
+    public static void serviceConsumer  (ServiceFactory fact){
+        Service s = fact.getService();
+        s.method1();
+        s.method2();
+    }
+
+    public static void main(String[] args) {
+        serviceConsumer(new Service1Factory());
+        // 服务是无安全可以互换的
+        serviceConsumer(new Service2Factory());
+    }
+}
