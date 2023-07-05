a90cca13d8c9eac160688c024c7c564fd0c9031a
gaoxiang <gaoxiang104@gmail.com>
2023-07-05 18:01:58 +0800

    gaoxiang : On Java 第11章笔记更新

---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/chapter11_innerclass/Callbacks.java
@@ -0,0 +1,87 @@
+package org.gx.onjava.chapter11_innerclass;
+// 使用内部类支持回调
+
+interface Incrementable { // 可增加
+    void increment();  // 增加
+}
+
+class Callee1 implements Incrementable {
+    private int i = 0;
+
+    @Override
+    public void increment() {
+        i++;
+        System.out.println(i);
+    }
+}
+
+class MyIncrement {
+    public void increment() {
+        System.out.println("Other operation");
+    }
+
+    static void f(MyIncrement mi) {
+        mi.increment();
+    }
+}
+
+class Callee2 extends MyIncrement {
+    private int i = 0;
+
+    @Override
+    public void increment() {
+        super.increment();
+        i++;
+        System.out.println(i);
+    }
+
+    // 如果我们的类必须以其他某种方式实现 increment()
+    // 则必须使用内部类
+    private class Closure implements Incrementable {
+        @Override
+        public void increment() {
+            // 需要指定调用外围类方法，否则会无限递归：
+            Callee2.this.increment();
+        }
+    }
+
+    Incrementable getCallbackReference() {
+        return new Closure();
+    }
+}
+
+class Caller {
+    private Incrementable callbackReference;
+
+    public Caller(Incrementable callbackReference) {
+        this.callbackReference = callbackReference;
+    }
+
+    void go() {
+        callbackReference.increment();
+    }
+}
+
+public class Callbacks {
+    public static void main(String[] args) {
+        Callee1 c1 = new Callee1();
+        Callee2 c2 = new Callee2();
+        MyIncrement.f(c2);
+        Caller caller1 = new Caller(c1);
+        Caller caller2 = new Caller(c2.getCallbackReference());
+        caller1.go();
+        caller1.go();
+        caller2.go();
+        caller2.go();
+    }
+}
+/* OUTPUT:
+Other operation
+1
+1
+2
+Other operation
+2
+Other operation
+3
+ */
