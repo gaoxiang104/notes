18266b5d2375d48e04391323bf67c5ba61486b22
gaoxiang <gaoxiang104@gmail.com>
2023-07-19 17:48:26 +0800

    gaoxiang : On Java 第12章笔记更新

---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/charpter12_collections/IterableClass.java
@@ -0,0 +1,34 @@
+package org.gx.onjava.charpter12_collections;
+
+import java.util.Iterator;
+
+// 任何实现 Iterable 接口的类都可以配合 for-in 使用
+public class IterableClass implements Iterable<String>{
+    protected String[] words =
+            "And that is how we know the Earth to be banana-shaped".split(" ");
+    @Override
+    public Iterator<String> iterator() {
+        return new Iterator<String>() {
+            private int index = 0;
+            @Override
+            public boolean hasNext() {
+                return index < words.length;
+            }
+
+            @Override
+            public String next() {
+                return words[index++];
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        for (String s : new IterableClass()) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+    }
+}
+/* output:
+And that is how we know the Earth to be banana-shaped
+ */
