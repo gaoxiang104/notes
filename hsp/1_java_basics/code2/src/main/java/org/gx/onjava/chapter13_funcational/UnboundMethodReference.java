116dacbb58bccaf308590d945f34c3adcc455ab0
gaoxiang <gaoxiang104@gmail.com>
2023-07-26 16:41:12 +0800

    gaoxiang : On Java 第13章笔记更新

---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/chapter13_funcational/UnboundMethodReference.java
@@ -0,0 +1,30 @@
+package org.gx.onjava.chapter13_funcational;
+// 未绑定对象的方法引用
+
+class X {
+    String f() {
+        return "X::f()";
+    }
+}
+
+interface MakeString{
+    String make();
+}
+
+interface TransformX{
+    String transform(X x);
+}
+
+public class UnboundMethodReference {
+    public static void main(String[] args) {
+        // MakeString ms = X::f; // [1]
+        TransformX sp = X::f;
+        X x = new X();
+        System.out.println(sp.transform(x)); // [2]
+        System.out.println(x.f());
+    }
+}
+/*
+X::f()
+X::f()
+ */
