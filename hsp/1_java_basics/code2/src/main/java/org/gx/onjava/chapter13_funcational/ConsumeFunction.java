09792b8d72c9e7965263bbb78e715168e8ae61cf
gaoxiang <gaoxiang104@gmail.com>
2023-07-28 23:32:29 +0800

    gaoxiang : On Java 第13章笔记更新

---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/chapter13_funcational/ConsumeFunction.java
@@ -0,0 +1,16 @@
+package org.gx.onjava.chapter13_funcational;
+
+import java.util.function.Function;
+
+class One {}
+class Two {}
+
+public class ConsumeFunction {
+    static Two consume(Function<One, Two> oneTwo) {
+        return oneTwo.apply(new One());
+    }
+
+    public static void main(String[] args) {
+        Two two = consume(one -> new Two());
+    }
+}
---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/chapter13_funcational/ProduceFunction.java
@@ -0,0 +1,16 @@
+package org.gx.onjava.chapter13_funcational;
+
+import java.util.function.Function;
+
+interface FuncSS extends Function<String,String> {} // [1]
+
+public class ProduceFunction {
+    static FuncSS produce() {
+        return s -> s.toLowerCase(); // [2]
+    }
+
+    public static void main(String[] args) {
+        FuncSS f =produce();
+        System.out.println(f.apply("YELLING"));
+    }
+}
