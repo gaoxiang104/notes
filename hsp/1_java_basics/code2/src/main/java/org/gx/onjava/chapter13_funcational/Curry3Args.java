c032ee9750e0bd5282a61843f514b61073e0af6e
gaoxiang <gaoxiang104@gmail.com>
2023-07-31 16:27:49 +0800

    gaoxiang : On Java 第13章笔记更新

---
+++ hsp/1_java_basics/code2/src/main/java/org/gx/onjava/chapter13_funcational/Curry3Args.java
@@ -0,0 +1,18 @@
+package org.gx.onjava.chapter13_funcational;
+
+import java.util.function.Function;
+
+public class Curry3Args {
+    public static void main(String[] args) {
+        Function<String,
+                Function<String,
+                        Function<String,String >>> sum =
+                a -> b -> c -> a + b + c;
+
+        Function<String,
+                Function<String, String>> h1 = sum.apply("Hi ");
+
+        Function<String ,String> ho = h1.apply("Ho ");
+        System.out.println(ho.apply("Hup"));
+    }
+}
