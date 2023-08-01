519c8ce9ee933e76dc77a8af9646557e10ce1375
gaoxiang <gaoxiang104@gmail.com>
2023-08-01 17:25:25 +0800

    gaoxiang : On Java 第14章笔记更新

---
+++ readingNotes/On Java/code/src/main/java/pers/xgo/onjava/chapter14_streams/ImperativeRandoms.java
@@ -0,0 +1,18 @@
+package pers.xgo.onjava.chapter14_streams;
+
+import java.util.Random;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class ImperativeRandoms {
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        SortedSet<Integer> rints = new TreeSet<>();
+        while (rints.size() < 7) {
+            int r = rand.nextInt(20);
+            if (r < 5) continue;
+            rints.add(r);
+        }
+        System.out.println(rints);
+    }
+}
