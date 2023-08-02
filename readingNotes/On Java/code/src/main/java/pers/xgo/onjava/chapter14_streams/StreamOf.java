7090a4c78b134739bc39dc8ae16544b6de55ac00
gaoxiang <gaoxiang104@gmail.com>
2023-08-02 22:34:55 +0800

    gaoxiang : On Java 第14章笔记更新

---
+++ readingNotes/On Java/code/src/main/java/pers/xgo/onjava/chapter14_streams/StreamOf.java
@@ -0,0 +1,20 @@
+package pers.xgo.onjava.chapter14_streams;
+
+import java.util.stream.Stream;
+
+public class StreamOf {
+    public static void main(String[] args) {
+        record Bubble(Integer i) { }
+
+        Stream.of(new Bubble(1), new Bubble(2), new Bubble(3))
+                .forEach(System.out::println);
+
+        Stream.of("It's ", "a ", "wonderful ", "day ", "for ", "pie")
+                .forEach(System.out::print);
+
+        System.out.println();
+
+        Stream.of(3.14159, 2.718, 1.618)
+                .forEach(System.out::println);
+    }
+}
