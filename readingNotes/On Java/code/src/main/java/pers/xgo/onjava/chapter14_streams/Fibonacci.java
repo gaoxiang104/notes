306d05a76d28ceb98611df25f1638f1efe831793
gaoxiang <gaoxiang104@gmail.com>
2023-08-05 16:37:52 +0800

    gaoxiang : On Java 第14章笔记更新

--- readingNotes/On Java/code/src/main/java/pers/xgo/onjava/chapter14_streams/Fibonacci.java
+++ readingNotes/On Java/code/src/main/java/pers/xgo/onjava/chapter14_streams/Fibonacci.java
@@ -1,2 +1,17 @@
-package pers.xgo.onjava.chapter14_streams;public class Fibonacci {
+package pers.xgo.onjava.chapter14_streams;
+
+import java.util.stream.Stream;
+
+public class Fibonacci {
+    static int x = 1;
+
+    public static void main(String[] args) {
+        Stream.iterate(0, i -> {
+                    int result = x + i;
+                    x = i;
+                    return result;
+                }).skip(20) // 不使用前20个
+                .limit(10) // 然后从中取10个
+                .forEach(System.out::println);
+    }
 }
