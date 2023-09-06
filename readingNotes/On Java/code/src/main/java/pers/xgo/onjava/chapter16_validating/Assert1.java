03ee854ef625d2e393c20b5831baa67d39717f37
gaoxiang <gaoxiang104@gmail.com>
2023-09-06 22:36:14 +0800

    gaoxiang : On Java 第16章笔记更新

---
+++ readingNotes/On Java/code/src/main/java/pers/xgo/onjava/chapter16_validating/Assert1.java
@@ -0,0 +1,7 @@
+package pers.xgo.onjava.chapter16_validating;
+
+public class Assert1 {
+    public static void main(String[] args) {
+        assert false;
+    }
+}
---
+++ readingNotes/On Java/code/src/main/java/pers/xgo/onjava/chapter16_validating/Assert2.java
@@ -0,0 +1,7 @@
+package pers.xgo.onjava.chapter16_validating;
+
+public class Assert2 {
+    public static void main(String[] args) {
+        assert false : "Here's a message saying what happened";
+    }
+}
