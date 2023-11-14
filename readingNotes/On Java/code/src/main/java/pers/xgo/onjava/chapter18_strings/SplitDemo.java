a70b33b30b87c201c05172c3cabce345da0ad5a3
gaoxiang <gaoxiang104@gmail.com>
2023-11-14 17:58:05 +0800

    gaoxiang : Pattern.spilt() 相关

---
+++ readingNotes/On Java/code/src/main/java/pers/xgo/onjava/chapter18_strings/SplitDemo.java
@@ -0,0 +1,17 @@
+package pers.xgo.onjava.chapter18_strings;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class SplitDemo {
+    public static void main(String[] args) {
+        String input = "This!!unusual use!!of exclamation!!points";
+        System.out.println(Arrays.toString(Pattern.compile("!!").split(input)));
+        // 只执行前3次
+        System.out.println(Arrays.toString(Pattern.compile("!!").split(input, 3)));
+    }
+}
+/* output:
+[This, unusual use, of exclamation, points]
+[This, unusual use, of exclamation!!points]
+ */
