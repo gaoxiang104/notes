25f32509c85413c9cb9e122f0f2d973fccd17493
gaoxiang <gaoxiang104@gmail.com>
2024-02-20 16:13:28 +0800

    gaoxiang : 第19章反射相关笔记更新

---
+++ readingNotes/On Java/code/src/main/java/pers/xgo/onjava/chapter19_reflection/ClassCasts.java
@@ -0,0 +1,12 @@
+package pers.xgo.onjava.chapter19_reflection;
+
+class Building {}
+class House extends Building {}
+public class ClassCasts {
+    public static void main(String[] args) {
+        Building b = new House();
+        Class<House> houseType = House.class;
+        House h = houseType.cast(b);
+        h = (House) b;
+    }
+}
