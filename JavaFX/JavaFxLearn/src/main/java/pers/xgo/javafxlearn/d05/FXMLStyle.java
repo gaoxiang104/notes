16d228b557490c318ad3479c8a70c98c8e5370ec
gaoxiang <gaoxiang104@gmail.com>
2024-03-06 01:24:40 +0800

    gaoxiang: JavaFX 相关笔记更新

---
+++ JavaFX/JavaFxLearn/src/main/java/pers/xgo/javafxlearn/d05/FXMLStyle.java
@@ -0,0 +1,15 @@
+package pers.xgo.javafxlearn.d05;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FXMLStyle extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
